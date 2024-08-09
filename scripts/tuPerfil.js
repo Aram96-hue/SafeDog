5d27fac8e225793d84f2cd59f7ce09a2c9c8fdaf
ElCaballerodelaTierra <carlos.alberto.villarreal.noyola@gmail.com>
2024-08-09 00:53:32 -0600

    Se agregaron las opciones propuestas en la daily para los formularios.
    Co-authored-by: Mitz MV <MitziMartinez@users.noreply.github.com>

--- scripts/tuPerfil.js
+++ scripts/tuPerfil.js
@@ -27,3 +27,19 @@
   }, false);
 })();
 
+
+function convertToJson() {
+    let form = document.getElementById("contactForm");
+    let formData = {};
+    for (let i = 0; i < form.elements.length; i++) {
+      let element = form.elements[i];
+      if (element.type !== "submit" && element.type !== "button" && element.type !== "file") {
+        formData[element.name] = element.value;
+      }
+    }
+    let jsonData = JSON.stringify(formData);
+    localStorage.setItem('cuidadorDatosGenerales', jsonData);
+    // Redirigir o mostrar un mensaje de confirmación
+    alert('Respuestas guardadas con éxito!');
+    console.log(jsonData);
+  }
