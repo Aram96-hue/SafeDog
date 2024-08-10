9c7a69ea628b466e11593e0b88d195a369b20b4a
albertoespejel <42525704+albertoespejel@users.noreply.github.com>
2024-08-10 17:16:29 -0600

    json/validacion update

--- scripts/tuPerfil.js
+++ scripts/tuPerfil.js
@@ -1,12 +1,11 @@
 
-
 document.getElementById('contactForm').addEventListener('submit', function(event) {
   event.preventDefault();
   let form = event.target;
   let photoInput = document.getElementById('formFileMultipleOne');
   let feedback = document.getElementById('formFeedback');
   
- 
+  
   if (photoInput.files.length === 0) {
       photoInput.setCustomValidity("Invalid");
       photoInput.nextElementSibling.style.display = 'block';
@@ -14,8 +13,8 @@ document.getElementById('contactForm').addEventListener('submit', function(event
       photoInput.setCustomValidity("");
       photoInput.nextElementSibling.style.display = 'none';
   }
-  
 
+  
   if (!form.checkValidity()) {
       feedback.textContent = "Por favor, completa todos los campos requeridos.";
       feedback.classList.add('text-danger');
@@ -24,13 +23,31 @@ document.getElementById('contactForm').addEventListener('submit', function(event
       feedback.textContent = "Información enviada con éxito.";
       feedback.classList.remove('text-danger');
       feedback.classList.add('text-success');
+
+      function convertToJson() {
+          let formData = {};
+          for (let i = 0; i < form.elements.length; i++) {
+              let element = form.elements[i];
+              if (element.type !== "submit" && element.type !== "button" && element.type !== "file") {
+                  formData[element.name] = element.value;
+              }
+          }
+          let jsonData = JSON.stringify(formData);
+          localStorage.setItem('cuidadorDatosGenerales', jsonData);
+          // Redirigir o mostrar un mensaje de confirmación
+          alert('Respuestas guardadas con éxito!');
+          console.log(jsonData);
+      }
+
+      // Llama la funcion Json cuando se cumplio la validación
+      convertToJson();
   }
 });
 
 
 
 
-function convertToJson() {
+/*function convertToJson() {
     let form = document.getElementById("contactForm");
     let formData = {};
     for (let i = 0; i < form.elements.length; i++) {
@@ -45,3 +62,4 @@ function convertToJson() {
     alert('Respuestas guardadas con éxito!');
     console.log(jsonData);
   }
+*/
