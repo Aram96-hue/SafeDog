b2b558630e098988bb6dabb880e6bd376b3eb852
ann-dy2712 <andrea.galeana27.dev@gmail.com>
2024-08-15 00:41:44 -0600

    Lista creación de objetos Json, y validaciones completas de los campos requeridos

--- scripts/registroNuevoUsuario.js
+++ scripts/registroNuevoUsuario.js
@@ -1,163 +1,243 @@
-(() => {
-    'use strict';
+function validateNombre(input) {
+    let pattern = /^[A-Za-z\s]+$/;
+    let feedback = input.nextElementSibling;
 
-    const forms = document.querySelectorAll('.needs-validation');
+    if (!pattern.test(input.value)) {
+        input.setCustomValidity("Invalid");
+        feedback.textContent = "El nombre solo debe contener letras y espacios.";
+        feedback.classList.remove('valid-feedback');
+        feedback.classList.add('invalid-feedback');
+        feedback.style.display = 'block';
+    } else {
+        input.setCustomValidity("");
+        feedback.textContent = "Nombre válido.";
+        feedback.classList.remove('invalid-feedback');
+        feedback.classList.add('valid-feedback');
+        feedback.style.display = 'block';
+    }
+}
 
-    Array.from(forms).forEach(form => {
-        form.addEventListener('input', event => {
-            const target = event.target;
+function validateTelefono(input) {
+    let pattern = /^\d{10}$/;
+    let feedback = input.nextElementSibling;
 
-            if (target.id === 'nombre') {
-                validateNombre(target);
-            } else if (target.id === 'telefono') {
-                validateTelefono(target);
-            } else if (target.id === 'exampleInputEmail1') {
-                validateCorreo(target);
-            } else if (target.id === 'exampleInputPassword1') {
-                validatePassword(target);
-            } else if (target.id === 'exampleInputPassword2') {
-                validatePasswordConfirm(target);
-            }
-        }, false);
+    if (!pattern.test(input.value) || input.value.length !== 10) {
+        input.setCustomValidity("Invalid");
+        feedback.textContent = "Número de teléfono inválido.";
+        feedback.classList.remove('valid-feedback');
+        feedback.classList.add('invalid-feedback');
+        feedback.style.display = 'block';
+    } else {
+        input.setCustomValidity("");
+        feedback.textContent = "Número de teléfono válido.";
+        feedback.classList.remove('invalid-feedback');
+        feedback.classList.add('valid-feedback');
+        feedback.style.display = 'block';
+    }
+}
 
-        form.addEventListener('submit', event => {
-            event.preventDefault();  // Evita el envío del formulario para no recargar la página
-            event.stopPropagation(); // Detiene la propagación del evento
+function validateCorreo(input) {
+    let pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let feedback = input.nextElementSibling;
 
-            if (form.checkValidity()) {
-                form.classList.add('was-validated');
-                // Muestra el mensaje de éxito
-                document.getElementById('success-message').textContent = "Registro exitoso!";
-                document.getElementById('success-message').style.display = 'block';
-                // Aquí puedes agregar la lógica para enviar el formulario, si es necesario
-                console.log('Formulario enviado');
-            } else {
-                form.classList.add('was-validated');
-            }
-        }, false);
+    if (!pattern.test(input.value)) {
+        input.setCustomValidity("Invalid");
+        feedback.textContent = "El correo electrónico no es válido.";
+        feedback.classList.remove('valid-feedback');
+        feedback.classList.add('invalid-feedback');
+        feedback.style.display = 'block';
+    } else {
+        input.setCustomValidity("");
+        feedback.innerHTML = "Correo electrónico válido.";  
+        feedback.classList.remove('invalid-feedback');
+        feedback.classList.add('valid-feedback');
+        feedback.style.display = 'block';
+    }
+}
+
+function validatePassword(input) {
+    const requirements = [
+        { regex: /.{8,}/, index: 0, message: "Debe tener al menos 8 caracteres" },
+        { regex: /[0-9]/, index: 1, message: "Debe tener al menos un número" },
+        { regex: /[a-z]/, index: 2, message: "Debe tener al menos una letra minúscula" },
+        { regex: /[!#$%&*?_@.\-]/, index: 3, message: "Debe tener al menos un carácter especial" },
+        { regex: /[A-Z]/, index: 4, message: "Debe tener al menos una letra mayúscula" },
+    ];
+
+    const requierementList = [
+        document.getElementById("req1"),
+        document.getElementById("req2"),
+        document.getElementById("req3"),
+        document.getElementById("req4"),
+        document.getElementById("req5")
+    ];
+
+    const password = input.value;
+
+    document.getElementById('requirements').classList.remove('hidden');
+
+    let allRequirementsMet = true;
+
+    requirements.forEach(item => {
+        const isValid = item.regex.test(password);
+        const requirementItem = requierementList[item.index];
+        if (isValid) {
+            requirementItem.textContent = `✅ ${item.message}`;
+            requirementItem.style.color = "green";
+        } else {
+            requirementItem.textContent = `❌ ${item.message}`;
+            requirementItem.style.color = "red";
+            allRequirementsMet = false;
+        }
     });
 
-    function validateNombre(input) {
-        let pattern = /^[A-Za-z\s]+$/;
-        let feedback = input.nextElementSibling;
-
-        if (!pattern.test(input.value)) {
-            input.setCustomValidity("Invalid");
-            feedback.textContent = "El nombre solo debe contener letras y espacios.";
-            feedback.classList.remove('valid-feedback');
-            feedback.classList.add('invalid-feedback');
-            feedback.style.display = 'block';
-        } else {
-            input.setCustomValidity("");
-            feedback.textContent = "Nombre válido.";
-            feedback.classList.remove('invalid-feedback');
-            feedback.classList.add('valid-feedback');
-            feedback.style.display = 'block';
-        }
+    if (allRequirementsMet) {
+        input.setCustomValidity("");
+    } else {
+        input.setCustomValidity("Invalid");
     }
+}
 
-    function validateTelefono(input) {
-        let pattern = /^\d{10}$/;
-        let feedback = input.nextElementSibling;
+function validatePasswordConfirm(input) {
+    const password = document.getElementById('exampleInputPassword1').value;
+    const confirmPassword = input.value;
+    const feedback = input.nextElementSibling;
 
-        if (!pattern.test(input.value) || input.value.length !== 10) {
-            input.setCustomValidity("Invalid");
-            feedback.textContent = "Número de teléfono inválido.";
-            feedback.classList.remove('valid-feedback');
-            feedback.classList.add('invalid-feedback');
-            feedback.style.display = 'block';
-        } else {
-            input.setCustomValidity("");
-            feedback.textContent = "Número de teléfono válido.";
-            feedback.classList.remove('invalid-feedback');
-            feedback.classList.add('valid-feedback');
-            feedback.style.display = 'block';
-        }
+    if (password !== confirmPassword) {
+        input.setCustomValidity("Passwords do not match");
+        feedback.textContent = "Las contraseñas no coinciden.";
+        feedback.classList.remove('valid-feedback');
+        feedback.classList.add('invalid-feedback');
+        feedback.style.display = 'block';
+    } else {
+        input.setCustomValidity("");
+        feedback.textContent = "";
+        feedback.classList.remove('invalid-feedback');
+        feedback.classList.add('valid-feedback');
+        feedback.style.display = 'none';
     }
+}
 
-    function validateCorreo(input) {
-        let pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        let feedback = input.nextElementSibling;
+// Add event listeners
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.needs-validation');
 
-        if (!pattern.test(input.value)) {
-            input.setCustomValidity("Invalid");
-            feedback.textContent = "El correo electrónico no es válido.";
-            feedback.classList.remove('valid-feedback');
-            feedback.classList.add('invalid-feedback');
-            feedback.style.display = 'block';
-        } else {
-            input.setCustomValidity("");
-            feedback.textContent = "Correo electrónico válido.";
-            feedback.classList.remove('invalid-feedback');
-            feedback.classList.add('valid-feedback');
-            feedback.style.display = 'block';
+    form.addEventListener('input', event => {
+        const target = event.target;
+
+        if (target.id === 'nombre') {
+            validateNombre(target);
+        } else if (target.id === 'telefono') {
+            validateTelefono(target);
+        } else if (target.id === 'exampleInputEmail1') {
+            validateCorreo(target);
+        } else if (target.id === 'exampleInputPassword1') {
+            validatePassword(target);
+        } else if (target.id === 'exampleInputPassword2') {
+            validatePasswordConfirm(target);
         }
-    }
+    }, false);
 
-    function validatePassword(input) {
-        const requirements = [
-            { regex: /.{8,}/, index: 0, message: "Debe tener al menos 8 caracteres" },
-            { regex: /[0-9]/, index: 1, message: "Debe tener al menos un número" },
-            { regex: /[a-z]/, index: 2, message: "Debe tener al menos una letra minúscula" },
-            { regex: /[!#$%&*?_@.\-]/, index: 3, message: "Debe tener al menos un carácter especial" },
-            { regex: /[A-Z]/, index: 4, message: "Debe tener al menos una letra mayúscula" },
-        ];
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        event.stopPropagation();
 
-        const requierementList = [
-            document.getElementById("req1"),
-            document.getElementById("req2"),
-            document.getElementById("req3"),
-            document.getElementById("req4"),
-            document.getElementById("req5")
-        ];
+        if (form.checkValidity()) {
+            form.classList.add('was-validated');
+            document.getElementById('success-message').textContent = "Registro exitoso!";
+            document.getElementById('success-message').style.display = 'block';
+            
+            const username = form.querySelector('#nombre').value;
+            const correo = form.querySelector('#exampleInputEmail1').value;
+            const telefono = form.querySelector('#telefono').value;
+            const password2 = form.querySelector('#exampleInputPassword1').value;
 
-        const password = input.value;
+            const newUser = {
+                username,
+                correo,
+                telefono,
+                password2
+            };
 
-        document.getElementById('requirements').classList.remove('hidden');
-
-        let allRequirementsMet = true;
-
-        requirements.forEach(item => {
-            const isValid = item.regex.test(password);
-            const requirementItem = requierementList[item.index];
-            if (isValid) {
-                requirementItem.textContent = `✅ ${item.message}`;
-                requirementItem.style.color = "green";
-            } else {
-                requirementItem.textContent = `❌ ${item.message}`;
-                requirementItem.style.color = "red";
-                allRequirementsMet = false;
-            }
-        });
-
-        if (allRequirementsMet) {
-            input.setCustomValidity("");  // Clear custom validity message if all requirements are met
+            let users = JSON.parse(localStorage.getItem('myForm2')) || [];
+            users.push(newUser);
+            localStorage.setItem('myForm2', JSON.stringify(users));
+            
+            console.log('Formulario enviado');
         } else {
-            input.setCustomValidity("Invalid");  // Set custom validity message if any requirement is not met
+            form.classList.add('was-validated');
         }
-    }
+    }, false);
+});
 
-    function validatePasswordConfirm(input) {
-        const password = document.getElementById('exampleInputPassword1').value;
-        const confirmPassword = input.value;
-        const feedback = input.nextElementSibling;
 
-        if (password !== confirmPassword) {
-            input.setCustomValidity("Passwords do not match");
-            feedback.textContent = "Las contraseñas no coinciden.";
-            feedback.classList.remove('valid-feedback');
-            feedback.classList.add('invalid-feedback');
-            feedback.style.display = 'block';
-        } else {
-            input.setCustomValidity("");
-            feedback.textContent = "";
-            feedback.classList.remove('invalid-feedback');
-            feedback.classList.add('valid-feedback');
-            feedback.style.display = 'none';
-        }
-    }
-})();
 
+ // Creación de multiples usuarios en el localStorage
+ function createUsersJsonTest() {
+    let usersJsonTest = [
+        {
+            "correo": "juanperez@example.com",
+            "password2": "Contraseña.123",
+            "telefono": "1122334455",
+            "username": "Juan Pérez"
+          },
+          {
+            "correo": "maria.lopez@example.com",
+            "password2": "Segura.456",
+            "telefono": "2233445566",
+            "username": "María López"
+          },
+          {
+            "correo": "carlos.martinez@example.com",
+            "password2": "MiClave.789",
+            "telefono": "3344556677",
+            "username": "Carlos Martínez"
+          },
+          {
+            "correo": "luisa.garcia@example.com",
+            "password2": "ClaveFuerte.101",
+            "telefono": "4455667788",
+            "username": "Luisa García"
+          },
+          {
+            "correo": "andrea.moreno@example.com",
+            "password2": "ContraseñaSegura.202",
+            "telefono": "5566778899",
+            "username": "Andrea Moreno"
+          },
+          {
+            "correo": "pedro.alvarez@example.com",
+            "password2": "Password.303",
+            "telefono": "6677889900",
+            "username": "Pedro Álvarez"
+          },
+          {
+            "correo": "sandra.jimenez@gmail.com",
+            "password2": "ClavePrivada.404",
+            "telefono": "7788990011",
+            "username": "Sandra Jiménez"
+          },
+          {
+            "correo": "jorge.mora@@gmail.com",
+            "password2": "MiPass.505",
+            "telefono": "8899001122",
+            "username": "Jorge Mora"
+          },
+          {
+            "correo": "elena.ramirez@@gmaile}.com",
+            "password2": "SafePassword.606",
+            "telefono": "9900112233",
+            "username": "Elena Ramírez"
+          },
+          {
+            "correo": "natalia.castillo@gmail.com",
+            "password2": "Secure.707",
+            "telefono": "0011223344",
+            "username": "Natalia Castillo"
+          }
+        ]
+        localStorage.setItem("usersTest", JSON.stringify(usersJsonTest));
+  };
 
 
 
