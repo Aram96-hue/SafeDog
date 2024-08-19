b652c3ba165ab8724804e1b7629b8e693704a660
ErickERC <erguez.eerc@gmail.com>
2024-08-19 10:14:13 -0600

    (?) No se porque se empeña en decirme que cambie el NavBar.js cuando no lo hice, ademas, cuando hago git restore no hace nada, me obliga a subir este cambio inexistente

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,5 +1,7 @@
 // Navbar
 
+
+
 window.addEventListener("load", () => {
     document.querySelector(".menu-btn").addEventListener("click", () => {
         document.querySelector(".mobile-menu").classList.toggle("active");
