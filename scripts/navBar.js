9071eb7a1aeddbf08f53829c8f7b327aedefa9b4
albertoespejel <42525704+albertoespejel@users.noreply.github.com>
2024-08-27 18:00:45 -0600

    mainLogin responsive

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,6 +1,13 @@
-window.addEventListener("load", () => {
+/*window.addEventListener("load", () => {
     document.querySelector(".menu-btn").addEventListener("click", () => {
         document.querySelector(".mobile-menu").classList.toggle("active");
+    });
+});
+*/
+
+window.addEventListener("load", () => {
+    document.querySelector("#menu-icon").addEventListener("click", () => {
+        document.querySelector("#dropdown-menu").classList.toggle("active");
     });
 });
 
