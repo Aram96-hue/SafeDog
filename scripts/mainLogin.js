494caf8f16c5f661cbe03618910e1fd9d4ba3207
ElCaballerodelaTierra <carlos.alberto.villarreal.noyola@gmail.com>
2024-08-27 20:01:49 -0600

    Se añadieron las páginas de la rama de Diego y se corrigieron estilos

--- scripts/mainLogin.js
+++ scripts/mainLogin.js
@@ -1,57 +1,36 @@
-  document
-  .getElementById("login-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
-
-    /**
-     * Aquí va el codigo referente a las validaciones con js de la forma:
-     * if(validation === erroneo){
-     *  return;
-     * }
-     */
-
-    
-//funcion de validacion de email
 function isEmailValid(email) {
-    let correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return correoRegex.test(email);
-  }
-
-//if para indicar en tiempo real si es valido (correo)
-document.getElementById("email").addEventListener("input", function () {
-    let correo = this.value.trim();
-    let feedback = document.getElementById("email-feedback");
-  
-    if (!isEmailValid(correo)) {
-      feedback.textContent = "❌ Correo electrónico inválido";
-      feedback.style.color = "red";
-    } else {
-      feedback.textContent = "✅ Correo electrónico válido";
-      feedback.style.color = "white";
-    }
-  });
-  
-//--------------------------- Contraseña--------------------------------------
-
+  let correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return correoRegex.test(email);
+}
 
 const requirements = [
-    {regex: /.{8,}/, index: 0, message: "Debe tener al menos 8 caracteres"}, // Minimo 8 caracteres.
-    {regex: /[0-9]/, index: 1, message: "Debe tener al menos un numero"}, // Al menos un numero.
-    {regex: /[a-z]/, index: 2,message: "Debe tener al menos una letra minúscula"}, // Al menos una letra minúscula
-    {regex: /[!#$%&*?_@-.]/, index: 3, message: "Debe tener al menos un caracter especial"}, // Al menos un caracter especial
-    {regex: /[A-Z]/, index:4, message: "Debe tener al menos una letra mayúscula"}, // Al menos una letra mayúscula
+  {regex: /.{8,}/, index: 0, message: "Debe tener al menos 8 caracteres"}, // Minimo 8 caracteres.
+  {regex: /[0-9]/, index: 1, message: "Debe tener al menos un numero"}, // Al menos un numero.
+  {regex: /[a-z]/, index: 2,message: "Debe tener al menos una letra minúscula"}, // Al menos una letra minúscula
+  {regex: /[!#$%&*?_@.\-]/, index: 3, message: "Debe tener al menos un caracter especial"}, // Al menos un caracter especial
+  {regex: /[A-Z]/, index:4, message: "Debe tener al menos una letra mayúscula"}, // Al menos una letra mayúscula
 ];
 
 const requierementList = [
-    document.getElementById("req1"),
-    document.getElementById("req2"),
-    document.getElementById("req3"),
-    document.getElementById("req4"),
-    document.getElementById("req5")
+  document.getElementById("req1"),
+  document.getElementById("req2"),
+  document.getElementById("req3"),
+  document.getElementById("req4"),
+  document.getElementById("req5")
 ];
 
+document.getElementById("username").addEventListener("input", function () {
+  let correo = this.value.trim();
+  let feedback = document.getElementById("email-feedback");
+
+  if (!isEmailValid(correo)) {
+    feedback.textContent = "❌ Correo electrónico inválido";
+    feedback.style.color = "red";
+  } else {
+    feedback.textContent = "✅ Correo electrónico válido";
+    feedback.style.color = "green";
+  }
+});
 
 const passwordInput = document.getElementById("password");
 passwordInput.addEventListener("keyup", (e) => {
@@ -69,11 +48,18 @@ passwordInput.addEventListener("keyup", (e) => {
     });
 });
 
-togglePassword.addEventListener('click', (e)=>{
-    const type = password.getAttribute('type') === 'password' ? 'text': 'password';
-    password.setAttribute('type', type);
-    e.target.classList.toggle('bi-eye');
+document.getElementById("togglePassword").addEventListener('click', (e)=>{
+  const type = password.getAttribute('type') === 'password' ? 'text': 'password';
+  password.setAttribute('type', type);
+  e.target.classList.toggle('bi-eye');
 });
+
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
     // Abajo la función de crear un usaurio dentro de localStorage para test
     createUsersJsonTest();
@@ -82,15 +68,9 @@ togglePassword.addEventListener('click', (e)=>{
     let existUser = false;
     let userIncorrect = true;
     usersTest.forEach((user) => {
-      console.log(user.email);
-      console.log(user.password);
-      console.log("--------------------------------");
-      console.log(email.value);
-      console.log(password.value);
-
       if (
-        user.email === email.value &&
-        user.password === password.value
+        user.username === username &&
+        user.password === password
       ) {
         // Aquí va el código referente a enviar al usuario con el resto de la página, se agrega un alert personalizado para ver que funciona esta parte
         userIncorrect = false;
@@ -98,14 +78,20 @@ togglePassword.addEventListener('click', (e)=>{
           icon: "success",
           title: "Bienvenido",
           text: "Usario y Contraseña validos",
-          confirmButtonText: 'Aceptar',
+          confirmButtonText: "Aceptar",
           confirmButtonColor: "#7f636e",
           timer: 1500,
+        
+        }).then(() => {
+
+          window.location.href = "../inicio.html";
+          //location.replace("tuPerfil.html");
         });
         return;
-      } else if (user.email === email.value) {
+      } else if (user.username === username) {
         existUser = true;
       }
+
     });
     if (userIncorrect) {
       Swal.fire({
@@ -114,8 +100,8 @@ togglePassword.addEventListener('click', (e)=>{
         text: existUser
           ? "Contraseña incorrecta, favor de introducir nuevamente tu contraseña"
           : "Usario y contraseña incorrectos, favor de intentar nuevamente",
-        confirmButtonText: 'Aceptar',
-        confirmButtonColor: "#7f636e"
+        confirmButtonText: "Aceptar",
+        confirmButtonColor: "#7f636e",
       });
     }
   });
@@ -124,46 +110,45 @@ togglePassword.addEventListener('click', (e)=>{
 function createUsersJsonTest() {
   let usersJsonTest = [
     {
-      email: "duarte.pablo@fernandes.com",
-      password: "VnwXqEDlnE1H3kO",
+      username: "duarte.pablo@fernandes.com",
+      password: "VnwXqEDl*nE1H3kO",
     },
     {
-      email: "daniel95@saraiva.net",
-      password: "129dSNxTwJiVrbI",
+      username: "daniel95@saraiva.net",
+      password: "129dSNxTwJiVrbI?",
     },
     {
-        email: "manuela.prado@ig.com.br",
-      password: "LpMwAv60h1nWzrm",
+      username: "manuela.prado@ig.com.br",
+      password: "LpMwAv6&0h1nWzrm",
     },
     {
-        email: "melissa.faria@matias.net.br",
-      password: "58CAIxzlUN4qhRV",
+      username: "melissa.faria@matias.net.br",
+      password: "58CAIx!zlUN4qhRV",
     },
     {
-        email: "lorena.castro@gmail.com",
-      password: "Nd1uEt57UtK3R72",
+      username: "lorena.castro@gmail.com",
+      password: "Nd1uEt5$7UtK3R72",
     },
     {
-        email: "hdasdores@hotmail.com",
-      password: "2DejABN6JVYRPNV",
+      username: "hdasdores@hotmail.com",
+      password: "2DejABN6JVYR#PNV",
     },
     {
-        email: "ferreira.thalissa@uol.com.br",
-      password: "7ShgoE3ET4ltkoY",
+      username: "ferreira.thalissa@uol.com.br",
+      password: "7ShgoE3ET4l@tkoY",
     },
     {
-        email: "ivan21@r7.com",
-      password: "AmDQcxPshML6zub",
+      username: "ivan21@r7.com",
+      password: "AmDQcxPshML6zub?",
     },
     {
-        email: "galindo.luzia@ferminiano.br",
-      password: "eDn8uVWP4gGdB3f",
+      username: "galindo.luzia@ferminiano.br",
+      password: "eDn8uVWP%4gGdB3f",
     },
     {
-        email: "asalas@correia.com.br",
-      password: "E6ZhTO6VcbRoNtL",
+      username: "asalas@correia.com.br",
+      password: "E6ZhTO6V_cbRoNtL",
     },
   ];
   localStorage.setItem("usersTest", JSON.stringify(usersJsonTest));
-}
-
+};
